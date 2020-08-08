eba859e3372bc351ab48f3d52bb16b6bbaeda505
Han <yinhan@csu.fullerton.edu>
2020-08-07 23:00:18 -0700

    M3 outputs -- CD B_R2

--- Microbiome/Metaphlan scripts/meta.sh
+++ Microbiome/Metaphlan scripts/meta.sh
@@ -1,30 +1,55 @@
 #!/bin/bash 
 
 
-folder='CD_paper'
+folder='CD_paper'     
+replicate='*_B_R2'
 
+
+# -----------------    set up directories    ------------------ #
+
+# home directory 
 home='/Volumes/Han_SSD'
+
+# working directories for input and output data
 in_dir=${home}/${folder}
 out_dir=${home}/output/${folder}
 
+
+
+# -----------------    metaphlan functions    -----------------  # 
+
+# activate conda environment
 source activate mpa 
 
-
+# function to run metaphlan script
 function run_metaphlan () 
-{
-	metaphlan ${in_dir}/$1.fastq.gz --input_type fastq --no_map -o ${out_dir}/$1.txt
+{ 
+	metaphlan ${in_dir}/$1.fastq.gz --input_type fastq --no_map -o ${out_dir}/$1.txt 
 }
 
-# run_metaphlan 'test'
 
-for path in ${in_dir}/C*_A_R2.fastq.gz; do
 
+# ---------------   for-loop over the folder   ---------------  # 
+
+# print start time 
+start_time=$(date)
+printf '\n======================   SCRIPT STARTS @ %s  ======================\n\n\n' "$start_time"
+
+
+for path in ${in_dir}/${replicate}.fastq.gz; do
+    
+    # extract file names without extensions (fastq.gz)
 	file=$(basename $path)
 	filename=${file%%.*}
 
+	# apply metaphlan script
 	run_metaphlan $filename
-	
-	printf '\t--------------------   %s DONE   -------------------\n\n\n' "$filename"
+
+	# create timestamp when job is done 
+	timestamp=$(date)
+
+	# print timestamp when job is done 
+	printf '\n--------------------   %s COMPLETED @ %s  -------------------\n\n\n' "$filename" "$timestamp"
 
 done 
 
